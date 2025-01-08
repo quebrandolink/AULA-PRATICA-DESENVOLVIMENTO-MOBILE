4dd0bca315d9b6cb2573b3033ea4ed6108e6f612
quebrandolink <quebrandolink@gmail.com>
2025-01-07 23:12:44 -0300

    primeira aula pratica

---
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hamburgueriaz;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}

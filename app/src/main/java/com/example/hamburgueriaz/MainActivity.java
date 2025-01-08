fe68c7b343bea7a43d4d7e6a512ebd5a262c0c21
quebrandolink <quebrandolink@gmail.com>
2025-01-08 00:10:28 -0300

    aula pratica 2 finalizada

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 }
